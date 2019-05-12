c808b48255d773812a27f0bfbf1aee49b66b46dd
Juji <rwkregime@gmail.com>
2019-05-11 23:01:39 -0700

    3.0 update
    major changes
    number swap mode added

--- proparser.sh
+++ proparser.sh
@@ -34,7 +34,7 @@ PS3='
 
 
  '
-options=("Domain Swap" "Add Numbers" "Case Swap" "Quit")
+options=("Domain Swap" "Add Numbers And Swap Case" "Number Swap" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -43,24 +43,23 @@ do
 read -e -p 'Bro Plz, enter that combo, Plz bro?: ' file
 ls -l '$file'
 
-  						echo 'Removing Gmail Domains'
+echo 'Removing All Strings Containing Gmail Domains'
 
+grep -riahv -E ".*@gmail.com.*" $file > gmailremoved.txt
 
-						grep -riahv -E ".*@gmail.com.*" $file > gmailremoved.txt
-
-						echo 'Swapping All Remaining Combo Domains To Gmail'
+echo 'Swapping Domain Of All Remaining Strings Containing Non-Gmail Domains To Gmail'
 			
-						sed -E "s/@.*:/@gmail.com:/" gmailremoved.txt > gmailadded.txt
-            		
+sed -E "s/@.*:/@gmail.com:/" gmailremoved.txt > gmailadded.txt            		
 
+echo 'Strings Successfully Domain Swapped'
 
-echo 'Combo Successfully Domainswapped'
-
-echo 'Deduping And Randomizing Combo'
+echo 'Deduping And Randomizing Strings Please Be Patient This May Take A While!!!'
 
 LC_ALL=C sort -Ru gmailadded.txt > domainswappedtarget.txt
 
 clear
+
+echo 'Strings Successfully Obfuscated!!!'
 
 echo 'Job Done, Cleaning Up Temporary Files'
 
@@ -85,99 +84,89 @@ DonJuji Loves You, Press Enter To Return To Menu'
 
 
             ;;
-        "Add Numbers")
+        "Add Numbers And Swap Case")
             
 
 read -e -p 'Bro Plz, enter that combo, Plz bro?: ' file
 ls -l '$file'
 
+echo 'Finding All Strings Containing Passwords Without Numeric Appendages' 
 
-grep -riahv -E ':.*[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9].*' $file > nonumbers.txt
+grep -riahv -E ':.*[a-z][0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9].*' $file > nonumbers.txt
 
-clear
+echo 'Finding All Strings Containing Uppercase Passwords Without Numeric Apendages'
 
 grep -rah -E '(:[A-Z])' nonumbers.txt > unparsedupper.txt
 
-echo 'Finding Lines Containing Passwords Without Numeric Appendages' 
+echo 'Swapping Case On All Strings Containing Uppercase Passwords Without Numeric Apendages'
 
-sed -e 's/\(:[A-Z]\)/\L\1/' unparsedupper.txt > parsedlower.txt
+sed -e 's/\(:[A-Z]\)/\L\1/' unparsedupper.txt >> parsedlower.txt
 
-echo 'Parsing Job 5% Done'
+echo 'Finding All Strings Containing Lowercase Passwords Without Numeric Apendages'
 
 grep -rah -E '(:[a-z])' nonumbers.txt > unparsedlower.txt
 
+echo 'Swapping All Strings Containing Lowercase Passwords Without Numeric Apendages'
+
 sed -e 's/\(:[a-z]\)/\U\1/' unparsedlower.txt > parsedupper.txt
 
-echo 'Swapping Case On First Letter Of Numeric-Absent-Appended Lines' 
-
-echo 'Parsing Job 10% Done'
+echo 'Adding All Strings Containing Passwords Withg Swapped Case Without Numeric Apendages Together'
 
 cat parsedupper.txt >> parsedmix.txt
 
 cat parsedlower.txt >> parsedmix.txt
 
-cat nonumbers.txt >> parsedmix.txt
-
-echo 'Parsing Job 15% Done'
+echo 'Adding 1 To All Strings Containing Passwords With Swapped Case Without Numeric Apendages'
 
 sed -E 's/(.*)/\11/' parsedmix.txt > parsedmix1added.txt
 
-echo 'Adding 1s To All Numeric-Absenst-Appended Lines'
+echo 'Adding 123s To All Strings Containing Passwords With Swapped Case Without Numeric Apendages'
 
 sed -E 's/(.*)/\1123/' parsedmix.txt > parsedmix123added.txt
 
-echo 'Parsing Job 25% Done'
+echo 'Adding All Strings Containing Paasswords With Swapped Case And New Numeric Appendeages Together'
 
 cat parsedmix1added.txt > numbersmix.txt
 
-echo 'Adding 123s To All Numeric-Absent-Ended Lines'
-
 cat parsedmix123added.txt >> numbersmix.txt
 
-echo 'Mixing 1 and 123 Appended Lines'
+echo 'Finding All Strings Containg Uppercase Passwords With Preexisting Numeric Apendages'
 
 grep -rah -E '(:[A-Z])' $file > unparsedupperall.txt
 
-echo 'Parsing Job 50% Done'
+echo 'Swapping Case On All Strings Containg Uppercase Passwords With Preexisting Numeric Apendages'
 
 sed -e 's/\(:[A-Z]\)/\L\1/' unparsedupperall.txt > parsedlowerall.txt
 
-echo 'Swapping Case On Pre-existing Numerically Appended Lines'
+echo 'Finding All Strings Containg Lowercase Passwords With Preexisting Numeric Apendages'
 
-grep -rah -E '(:[a-z])' $file.txt > unparsedlowerall.txt
+grep -rah -E '(:[a-z])' $file > unparsedlowerall.txt
 
-echo 'Parsing Job 70% Done'
+echo 'Swapping Case On All Strings Containg Uppercase Passwords With Preexisting Numeric Apendages'
 
 sed -e 's/\(:[a-z]\)/\U\1/' unparsedlowerall.txt > parsedupperall.txt
 
-echo 'Mixing Both Cases On Pre-existing Numerically Appended Lines'
+echo 'Adding All Strings Containg Passwords With Swapped Case And Preexisting Numeric Apendages Together'
 
 cat parsedupperall.txt > allmix.txt
 
-echo 'Parsing Job 90% Done'
-
 cat parsedlowerall.txt >> allmix.txt 
 
-echo 'Mixing All Created New Lines'
+echo 'Adding All New Strings Together'
 
 cat numbersmix.txt > finalmix.txt
 
 cat allmix.txt >> finalmix.txt
 
-clear
+echo 'Deduping And Randomizing Strings Please Be Patient This May Take A While!!!'
 
-echo 'Combo Successfully Obfuscated!!!'
-
-echo 'Deduping And Randomizing Combo'
-
-LC_ALL=C sort -Ru finalmix.txt > numberandcasetarget.txt
+LC_ALL=C sort -Ru finalmix.txt > caseswapped-numericallyappended-result.txt
 
 clear
+
+echo 'Strings Successfully Obfuscated!!!'
 
 echo 'Job Done, Cleaning Up Temporary Files'
-
-clear
-
 
 rm nonumbers.txt 
 
@@ -209,7 +198,6 @@ rm allmix.txt
 
 rm finalmix.txt 
 
-
 read -p '
 
                                           
@@ -229,55 +217,47 @@ DonJuji Loves You, Press Enter To Return To Menu'
 
             ;;
        
-        "Case Swap")
+        "Number Swap")
             
 
 read -e -p 'Bro Plz, enter that combo, Plz bro?: ' file
 ls -l '$file'
 
+echo 'Finding All Strings Containing Pre-Existing Numeric Apendages'
 
-grep -rah -E '(:[A-Z])' $file > unparsedupperall.txt
+grep -riah -E ':.*[a-z][0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9].*' $file > preexistingapendegaes.txt
 
-echo 'Parsing Job 10% Done'
+echo 'Swapping All Strings Containing Pre-Existing Numeric Apendage Of 1 To 123'
 
-sed -e 's/\(:[A-Z]\)/\L\1/' unparsedupperall.txt > parsedlowerall.txt
+sed -e 's/[0-9]+$/123/' preexistingapendegaes.txt > swappedto123.txt
 
-echo 'Swapping Case On All Lines'
+echo 'Swapping All Strings Containing Pre-Existing Numeric Apendage Of 123 To 1'
 
-grep -rah -E '(:[a-z])' $file.txt > unparsedlowerall.txt
+sed -e 's/[0-9]+$/1/' preexistingapendegaes.txt > swappedto1.txt
 
-echo 'Parsing Job 35% Done'
+echo 'Adding All Strngs Containing Newly Swapped Numeric Apendages Together'
 
-sed -e 's/\(:[a-z]\)/\U\1/' unparsedlowerall.txt > parsedupperall.txt
+cat swappedto123.txt > numswapmix.txt 
 
-echo 'Mixing Both Cases On All Lines'
+cat swappedto1.txt >> numswapmix.txt
 
-cat parsedupperall.txt > allmix.txt
+echo 'Deduping And Randomizing Strings Please Be Patient This May Take A While!!!'
 
-echo 'Parsing Job 90% Done'
-
-cat parsedlowerall.txt >> caseswapmix.txt 
-
-echo 'Combo Successfully Caseswapped'
-
-echo 'Deduping And Randomizing Combo'
-
-LC_ALL=C sort -Ru caseswapmix.txt > caseswappedtarget.txt
+LC_ALL=C sort -Ru numswapmix.txt > numberswappedresult.txt
 
 clear
 
+echo 'Strings Successfully Obfuscated!!!'
+
 echo 'Job Done, Cleaning Up Temporary Files'
 
-rm unparsedupperall.txt
+rm swappedto1.txt
 
-rm unparsedlowerall.txt
+rm swappedto123.txt
 
-rm parsedupperall.txt
+rm numswapmix.txt
 
-rm parsedlowerall.txt
-
-rm allmix.txt
-
+rm preexistingapendegaes.txt
 
 read -p '
 
